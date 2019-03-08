49dc46e702f4d02f1a14c0e781401e459f22ddd6
Mirza <mirza@Mirzas-MBP.lan>
2019-03-07 19:45:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  handleIncrement() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  
+  handleReset() {
+    alert('Do you really want to reset the counted number!');
+    this.setState({
+      count: 0
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
-        </header>
-      </div>
+    <div className="main">
+      <h1 className="header">Total Visitors at Art Gallery</h1>
+      <p className="count">{this.state.count}</p>
+      <button className='increment' onClick={(e) => this. handleIncrement()}> Increment</button>
+      <button className='reset' onClick={(e) => this.handleReset()}>Reset!</button>
+    </div>
     );
   }
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-if (module.hot) {
-    module.hot.accept();
-  }
